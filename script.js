7d6d0128be4e209f0691d713a3031629787332bd
Julia <Julia_roza@estudante.sesisenai.org.br>
2025-04-04 08:50:11 -0300

    add soma dos números que o usuário pedir

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 let n1 = 0
-let n3=0
-let soma=0
-let n = parseFloat(prompt ('quantos números você quer somar?'));
+let n3 = 0
+let soma = 0
+let n = parseFloat(prompt('quantos números você quer somar?'));
 
 do {
-    let n3 = parseFloat(prompt ('Digite um número'));
-    console.log("n:", n)
-    console.log( "n1:", n1)
-    
-    
-    console.log (soma + n3)
+    let n3 = parseFloat(prompt('Digite um número'));
+    soma = soma + n3
+    console.log(soma + n3)
     n1 = n1 + 1
 }
 while (n > n1);
