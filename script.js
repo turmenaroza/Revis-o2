f7d627e57cee873e9c64af4cd9720c9edb57e97e
Julia <Julia_roza@estudante.sesisenai.org.br>
2025-04-04 09:32:58 -0300

    add contagem regressiva

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-let n1 = 0
-let n3 = 0
-let soma = 0
-let n = parseFloat(prompt('quantos números você quer somar?'));
+//let n1 = 0
+//let n3 = 0
+//let soma = 0
+//let n = parseFloat(prompt('quantos números você quer somar?'));
 
-do {
-    let n3 = parseFloat(prompt('Digite um número'));
-    soma = soma + n3
-    console.log(soma + n3)
-    n1 = n1 + 1
-}
-while (n > n1);
+//do {
+    //let n3 = parseFloat(prompt('Digite um número'));
+    //soma = soma + n3
+    //console.log(soma + n3)
+    ///n1 = n1 + 1
+//}
+//while (n > n1);
+
+
+
+let intervalo = null;
+
+var timer = function(){
+    let count = 10;
+    intervalo = setInterval(() =>{
+        console.log(count);
+        count--
+        }, 1000)}
+
+var stop = function(){
+    setTimeout(() => {
+        clearInterval(intervalo)
+        console.log('Lançamento');        
+            }, 10000)
+    }
+
+timer();
+stop();
+
+
 
